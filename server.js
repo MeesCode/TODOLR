41bee92e883c74fc3cd6340d7b37aa4a1ad371e8
Mees Brinkhuis <M.Brinkhuis@student.tudelft.nl>
2015-12-11 18:26:28 +0100

    can now get info from database, breaks /change

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 var http = require("http");
 var express = require("express");
 var url = require("url");
+var mysql = require("mysql");
 var app = express();
 var idCounter = 0;
 
 http.createServer(app).listen(8080);
 app.use(express.static('static'));
+
+var connection = mysql.createConnection({
+  host     : "localhost",
+  user     : "root",
+  password : "merel4840",
+  database : "todo"
+});
+
+connection.connect(function(err) {
+  console.log("Connected to database");
+});
 
 var array = [];
 
@@ -21,7 +33,29 @@ function item(title, text, pic, tags, date, priority, done, archived, idCounter)
   this.idCounter = idCounter;
 }
 
+var getQuery = "SELECT Title AS title, ToDoItem.Text AS text, Pic AS pic, Tag.Text AS tags,"
+             +"DueDate AS date, Priority AS priority, Completed AS done, Archived AS archived, ToDoItem.Id-1 AS idCounter "
+             +"FROM ToDoItem, ItemTag, Tag "
+             +"WHERE ToDoItem.Id=ItemTag.ToDoId AND ItemTag.TagId=Tag.Id";
+
+var getFromDatabase = function(err, result) {
+  if (!err){
+    console.log("Database returned");
+
+    for(var i = 0; i < result.length; i++){
+      (result[i].priority == 1) ? result[i].priority = true : result[i].priority = false;
+      (result[i].done == 1) ? result[i].done = true : result[i].done = false;
+      (result[i].archived == 1) ? result[i].archived = true : result[i].archived = false;
+      array = result;
+    }
+
+  } else {
+    console.log("error with database connection: " + err);
+  }
+}
+
 app.get("/change", function(req, res){
+  console.log("change request");
   res.writeHead(200);
   var query = url.parse(req.url, true).query;
 
@@ -36,6 +70,7 @@ app.get("/change", function(req, res){
 });
 
 app.get("/set", function(req, res){
+  console.log("set request");
   res.writeHead(200);
   var query = url.parse(req.url, true).query;
   idCounter = array.length;
@@ -51,6 +86,8 @@ app.get("/set", function(req, res){
 });
 
 app.get("/get", function(req, res){
+  console.log("get request");
   res.writeHead(200);
+  connection.query(getQuery, getFromDatabase);
   res.end(JSON.stringify(array));
 });
